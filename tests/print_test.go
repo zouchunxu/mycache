20915984314472148a1b193354d4b9b6c7835aa9
Chunxu Zou <471500439@qq.com>
2021-08-10 21:52:15 +0800

    cache

---
+++ tests/print_test.go
@@ -0,0 +1,28 @@
+package tests
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+var m sync.Mutex
+
+var set = make(map[int]bool, 0)
+
+func printOnce(num int) {
+	m.Lock()
+	defer m.Unlock()
+	if _, exist := set[num]; !exist {
+		fmt.Println(num)
+	}
+	set[num] = true
+}
+
+func TestPrintOne(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		go printOnce(100)
+	}
+	time.Sleep(time.Second)
+}
